41f5dc2b628241bcd0a7298d1181855585b8a189
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-05-29 17:28:51 +0700

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+// import component tu thu vien reactstrap
+import { Navbar, NavbarBrand } from 'reactstrap';
+// la mot trong nhung thu vien trong he sinh thai cua react
 class App extends Component {
-
+// tao ra mot class component App tu Component cua react 
   render() {
     return (
+      // bat buoc phai co mot div hoac React.Fragment bao cac phan tu ben trong
       <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
+        {/* Navbar va NavbarBrand tuong tu voi navbar, navbarbrand trong bootstrap, su dung ngan gon hon trong bootstrap */}
+        <Navbar dark color="primary">
+          <div className='container'>
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
       </div>
     );
   }
 }
-
+// phai xuat Component ra thi moi co the import de su dung duoc Component do 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css';
-import 'bootstrap-social/bootstrap-social.css';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';  
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// ReactDOM la tao ra DOM ao de render ra Component App o DOM co id='root'.
 ReactDOM.render(
     <App />,
   document.getElementById('root')
