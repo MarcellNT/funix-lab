a87a24606a8250370cd71719e4f7692093e49734
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-06-04 16:36:31 +0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Main from './components/MainComponent';
 
 class App extends Component {
-// App trở thành presentaion component với chức năng render ra Main component
+// App trở thành presentaion component với chức năng render ra Main component container
   render() {
     return (   
       <div className="App">      
