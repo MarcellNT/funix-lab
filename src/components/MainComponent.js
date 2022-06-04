2e00e240d3c6b818f637ff180ca97c65c4f1cc5f
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-06-04 17:45:17 +0700

    Functional Components

---
+++ src/components/MainComponent.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './MenuComponent';
+import DishDetail from './DishDetailComponent ';
+import { DISHES } from '../shared/dishes';
+// /Main component là container component nơi lưu trữ các state, sử dụng presentation component và truyền props cho presentaion Menu và DishDetail 
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
+        };  
+    }
+
+    onDishSelect(dishId) {
+        this.setState({ selectedDish: dishId});
+    }
+
+    render() {
+        return(
+            <div>
+                <Navbar dark color="primary">
+                    <div className='container'>
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                {/* Truyền props cho component Menu */}
+                {/* Khi click và dish thì sẽ hiển thị thông tin chi tiết của dish đó và dish được lấy theo giá trị dishId */}
+                <Menu
+                    dishes={this.state.dishes}
+                    onClick={(dishId) => this.onDishSelect(dishId)}
+                />
+                {/* Truyền props cho component DishDetail */}
+                {/* Chỉ hiển thị đúng món ăn có dishid tương ứng với state là selectedDish được chọn */}
+                <DishDetail
+                    dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}
+                />
+            </div>
+        )
+    }
+}
+
+export default Main;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,77 +1,48 @@
-import React, { Component } from "react";
+import React from "react";
 import {  Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
-import DishDetail from "./DishDetailComponent ";
-class Menu extends Component {
-    // Luu state va truyen props
-    constructor(props) {
-        super(props);
-        // luu lai state cua component
-        this.state = {
-            // tạo một props 
-            selectedDish: null
-        }
-    }
-    onDishSelect(dish) {
-        this.setState({selectedDish: dish})
-        //  Cập nhât state component
-    }
-    // renderDish(dish) {
-    //     if(dish != null) {
-    //         return (
-    //             <div className="col-12 col-md-5 m-1">
-    //                 <Card>
-    //                 <CardImg width="100%" src={dish.image} alt={dish.name}/>
-    //                 <CardBody>
-    //                     <CardTitle>{dish.name}</CardTitle>
-    //                     <CardText>{dish.description}</CardText>
-    //                 </CardBody>
-    //             </Card>
-    //             </div> 
-                
-    //         );
-    //     }
-    //     // khi click selectedDish null thì sẽ trả về div rỗng và không render gì trên browser
-    //     else {
-    //         return (
-    //             <div></div>
-    //         ) ;       
-    //     }
-    // }
-    render() {
-        // Su dung ham map de duyet qua state   
-        const menu = this.props.dishes.map((dish) => {
-            return ( 
-                // key phai co gia tri la duy nhat
-                
-                <div className="col-12 col-md-5 m-1" key={dish.id}>
-                    <Card  
-                        // khi click vào bất kỳ một dishes nào sẽ hiện thông tin 
-                        onClick={() => this.onDishSelect(dish)}
-                        > 
-                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                        <CardImgOverlay>
-                            <CardTitle>{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>                           
+
+//function component RenderMenuItem nhận param là dish và onClick để render ra danh sách món ăn
+    function RenderMenuItem({dish, onClick}) {
+            return(
+                <Card 
+                    onClick={() => onClick(dish.id)}
+                >
+                    <CardImg
+                        width="100%"
+                        src ={dish.image}
+                        alt ={dish.name}
+                    />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Card>        
+            )
+    } 
+// function component Menu nhận props từ container class component cha Main để render ra danh sách món ăn nhưng không render ra thông tin chi tiết món ăn nữa
+// props nhận được ở đây là cả state dishes và function onClick    
+    const Menu = (props) => {
+        const menu = props.dishes.map((dish) => {
+            return(
+                <div className="col-12 col-md-5 m-1">
+                    <RenderMenuItem
+                        dish = {dish}
+                        onClick={props.onClick}
+                    />
                 </div>
+
             )
         })
-        return (   
+        return(
             <div className="container">
                 <div className="row">
-                        {menu}
-                </div>
-                <div className="row">
-                    {/* gọi renderDish function render dish  */}
-                    {/* {this.renderDish(this.state.selectedDish)} */}
-                </div>
-                <div>
-                    <DishDetail dish={this.state.selectedDish}/>
+                    {menu}
                 </div>
             </div>
         )
-    }
 
-}   
+    }  
+        
+   
+
 
 export default Menu;
