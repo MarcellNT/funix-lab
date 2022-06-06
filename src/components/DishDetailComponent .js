6bef29d6c6bf11f18bcbf8718bb50f330758f02b
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-06-06 17:32:19 +0700

    Single Page Applications Part 2

--- src/components/DishDetailComponent .js
+++ src/components/DishDetailComponent .js
@@ -1,61 +1,86 @@
 import React, { Component } from "react";
 import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
 } from "reactstrap";
-class DishDetail extends Component {
+import { Link } from "react-router-dom";
 
-    // sử dụng hàm map để duyệt qua comment lấy phần tử bên trong
-    renderComment(dish) {
-        if (dish.comments != null) {
-            const menu = dish.comments.map((comment) => {
-                // gan date lay gia tri date
-                let date = new Date(comment.date);
-                return (
-                    <div key={comment.id}>
-                        <p>{comment.comment}</p>
-                        {/* Chuyen thanh chuoi date thanh chuoi  */}
-                        <p>--{comment.author}, {date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
-                    </div>
-                );
-            });
-            return menu;
-        } else {
-            return (
-                <div></div>
-            )
-        }
-
-    }
-
-    render() {
-        const dish = this.props.dish;
-        if (dish != null) {
-            return (
-                // render ra giao dien
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <Card>
-                                <CardImg width="100%" src={dish.image} />
-                                <CardBody>
-                                    <CardTitle>{dish.name}</CardTitle>
-                                    <CardText>{dish.description}</CardText>
-                                </CardBody>
-                            </Card>
-                        </div>
-                        <div className="col-12 col-md-5 m-1 ">
-                            <h4>comment</h4>
-                            {this.renderComment(dish)}
-                        </div>
-                    </div>
-                </div>
-            )
-        } else {
-            return (
-                <div></div>
-            )
-        }
-    }
+//function component render ra thông tin chi tiết món ăn
+function RenderDish({ dish }) {
+  return (
+      <Card>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+  );
 }
+
+//function component render ra comment của khách hàng
+function RenderComments({ comments }) {
+  if (comments != null) {
+    const menu = comments.map((comment) => {
+      let date = new Date(comment.date);
+      return (
+        <div key={comment.id}>
+          <p>{comment.comment}</p>
+          <p>
+            -- {comment.author},{" "}
+            {date.toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </p>
+        </div>
+      );
+    });
+    return menu;
+  } else {
+    return <div></div>;
+  }
+}
+
+// container component truyền props cho function component là RenderDish và Rendercomment và nhận props từ component cha là Maincomponent
+const DishDetail = (props) => {
+  const dish = props.dish;
+
+  if (dish != null) {
+    return (
+      <div className="container">
+        <div className="row">
+          {/* Thêm breadcrumb */}
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <RenderComments comments={props.comments} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+};
+
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,10 @@ import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 
 // /Main component là container component nơi lưu trữ các state, sử dụng presentation component và truyền props cho presentaion Menu và DishDetail 
@@ -36,12 +40,19 @@ class Main extends Component {
                 />
             );
         }
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+            );
+        };
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
