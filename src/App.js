88964339ba6365c91271dcfd5116b4fa75c56fdb
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-06-19 16:47:09 +0700

    Controlled Forms

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
-
 class App extends Component {
   // App trở thành presentaion component với chức năng render ra Main component container
   render() {
